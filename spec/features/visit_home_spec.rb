db8f00c51c27c1830fce0a482f27ef33dd15d3ee
Francisco Felix <franciscofelixs@gmail.com>
2018-08-02 10:24:33 -0300

    [WIP] spec script

--- spec/features/visit_home_spec.rb
+++ spec/features/visit_home_spec.rb
@@ -7,4 +7,10 @@ feature 'visit homepage' do
 		expect(page).to have_css('h1', text: 'CookBook')
 		expect(page).to have_css('h3', text: 'Bem vindo ao maior livro de receitas online!')
 	end
+	scenario 'and view list recipe' do
+		recipe = Recipe.create(title: 'Feijuada', ingredients:'arroz, feijao, carnes', 
+													 recipe_types: 'Prato Principal', cuisines: 'brasileira',
+													 method: 'cozinhar o arroz, colocar feijão e carnes na panela de pressão', cook_time: 60 'minutos')
+
+  end
 end
